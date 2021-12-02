8fcf23c0f6d998c190ed205c0d94c26518835ff5
Josias <50959958+SirJoc@users.noreply.github.com>
2021-12-02 15:15:55 -0500

    feat: add function proof of work

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,13 +7,17 @@ public class App
 {
 
     private final String message = "Hello World!";
-
+    private final String proof = "Work";
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
 
+    private final String proofOfWork() {
+        return proof;
+    }
+    
     private final String getMessage() {
         return message;
     }
